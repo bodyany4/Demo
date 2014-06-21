700f7a427e27c21d74f65656c3267b8321510f45
bodyany4 <bodyany4@bk.ru>
2014-06-22 02:37:40 +0300

    Finished demo scaffold app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+Demo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+Demo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
